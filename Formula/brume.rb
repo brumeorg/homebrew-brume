ab980900d8ae84095015e15c3e32d554092eea6b
CI <ci@brumeorg.dev>
2026-09-15 10:26:53 +0000

    brume 1.4

--- Formula/brume.rb
+++ Formula/brume.rb
@@ -1,17 +1,17 @@
 class Brume < Formula
   desc "Brume is a pseudonymisation tool, that allows you to use coherent data and production data with GDPR compliance"
   homepage "https://github.com/brumeorg/Brume"
-  version "1.3"
+  version "1.4"
   license "Apache-2.0"
   on_macos do
     on_arm do
-      url "https://github.com/brumeorg/Brume/releases/download/1.3/brume_1.3_darwin_arm64.tar.gz"
-      sha256 "2d4c77b1efbbe27de51653e1ef6c38a86a031e404b9f89a8f4e6a9f996695581"
+      url "https://github.com/brumeorg/Brume/releases/download/1.4/brume_1.4_darwin_arm64.tar.gz"
+      sha256 "42a5a69089aad0fd0de0b7eeac467c936c18c850cefad6459cc0ebc0c773c381"
     end
   end
   on_linux do
-    url "https://github.com/brumeorg/Brume/releases/download/1.3/brume_1.3_linux_amd64.tar.gz"
-    sha256 "a5b5ecf6e6206f4860d81c8ed5a082691f04a44063e0767ba13182677c5177bd"
+    url "https://github.com/brumeorg/Brume/releases/download/1.4/brume_1.4_linux_amd64.tar.gz"
+    sha256 "f2d45fe92a1d28938795da8498c074776714df06e8d0c16cfb012aa6ed344cfc"
   end
   def install
     bin.install "brume"
